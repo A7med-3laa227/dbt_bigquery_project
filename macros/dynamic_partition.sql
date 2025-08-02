c17497e1430a762eb4452b987cae285bd4ae16b0
Ahmed Alaa <ahmedalaa65675@gmail.com>
2025-08-02 15:09:37 +0200

    updating function

--- macros/dynamic_partition.sql
+++ macros/dynamic_partition.sql
@@ -1,6 +1,5 @@
 {% macro dynamic_partition(column_name, interval) %}
     CASE 
-        WHEN DATE({{ column_name }}) >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 {{ interval }}) THEN 'recent'
         ELSE 'historical'
     END AS partition_group
 {% endmacro %}
